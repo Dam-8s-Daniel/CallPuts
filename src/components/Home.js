ea1cfff61da2961880b8ffba7917c3a8a7d57773
Daniel Dam <damd@oregonstate.edu>
2022-03-05 19:07:29 -0800

    changed hompage to include image and to change message

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,14 +2,21 @@ import React from 'react';
 import robot from '../robotHi.png'
 export default function Home () {
 
-
     return (
     <div className='Homepage'>
             <div className='WelcomeImg'><img src={robot}></img></div>
             <div className='WelcomeMessage'>
                 <h1>Welcome to CallPuts!</h1>
-                <p>Charts: Get a chart of closing prices from the last 3 months. Enter the stock you bought, the price, and the amount to get how much your investment increased. Fast, easy, and reliable.</p>
-                <p>Research: The best place to search for top news. Enter the company name to get a summary of the company from Wikipedia or search for the top current news stories.</p>      
+                <p>
+                    Charts: Get a chart of closing prices from the last 3 months. 
+                    Enter the stock you bought, the price, and the amount to get how much your investment increased. 
+                    Fast, easy, and reliable.
+                </p>
+                <p>
+                    Research: The best place to search for top news. 
+                    Enter the company name to get a summary of the company from Wikipedia or search for the top 
+                    current news stories.
+                </p>      
                 <p>Contact: Send me a message if you have any questions or just want to say hi! </p>  
             </div>    
     </div>
